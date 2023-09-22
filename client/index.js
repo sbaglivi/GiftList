f36b7239258254b7aae5789fe6c3a64681ca614b
Simone <sbaglivi@gmail.com>
2023-09-22 09:43:44 +0200

    Modified logic so that client sends a proof and his name (a random one from the list is picked every time). The server then uses proof and name to verify that the name is one of the leaves of the tree by computing the merkle root and comparing it to the one hardcoded.

--- client/index.js
+++ client/index.js
@@ -6,9 +6,14 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const tree = new MerkleTree(niceList);
+
+  const ourIndex = Math.floor(Math.random() * niceList.length);
+  const proof = tree.getProof(ourIndex);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    name: niceList[ourIndex]
   });
 
   console.log({ gift });
